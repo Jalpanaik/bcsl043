8bffd200481b0e8aea152bc0376e1675cda5fbc1
Jalpanaik <133594529+Jalpanaik@users.noreply.github.com>
2025-05-17 12:35:30 +0000

    version1

---
+++ s1.java
@@ -0,0 +1,9 @@
+public class s1 {
+    public static void main(String[] args) {
+        int h=10;
+        int w=20;
+        int area=h*w;
+        System.out.println("area is"+area);
+    }
+    
+}
