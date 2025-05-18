c3207f8cc36168a448a04ad3c7b84b2a19a81e18
Jalpanaik <133594529+Jalpanaik@users.noreply.github.com>
2025-05-18 06:00:59 +0000

    version1

--- s2.java
+++ s2.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
 public class s2
 {
     public static void main(String args[])
     {
-        int a=10,b=5;
-         System.out.println((a<<2) + (b>>2));
-         Sysetm.out.println(a+"||"+(b>0));
-         System.out.println((a+b*100)/10);
-         System.out.println(a+"&"+b);
+        Scanner in=new Scanner(System.in);
+        int marks[]=new int[5];
+        int total=0,i;
+        for(i=0;i<5;i++)
+        {
+            System.out.println("Enter marks:");
+            marks[i]=in.nextInt();
+            total=total+marks[i];
+        }
+        double avg=total/5.0;
+        System.out.println("Average amrks is:"+avg);
+        
     }
 }
