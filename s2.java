fff8b2d767b2d348c286fce58f34e0379a796759
Jalpanaik <133594529+Jalpanaik@users.noreply.github.com>
2025-05-18 05:36:30 +0000

    version1

---
+++ s2.java
@@ -0,0 +1,11 @@
+public class s2
+{
+    public static void main(String args[])
+    {
+        int a=10,b=5;
+         System.out.println((a<<2) + (b>>2));
+         Sysetm.out.println(a+"||"+(b>0));
+         System.out.println((a+b*100)/10);
+         System.out.println(a+"&"+b);
+    }
+}
