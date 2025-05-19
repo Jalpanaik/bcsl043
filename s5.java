141177f54b8dc50e159307a5db2e92a6e75c0da3
Jalpanaik <133594529+Jalpanaik@users.noreply.github.com>
2025-05-19 11:56:46 +0000

    v1

---
+++ s5.java
@@ -0,0 +1,30 @@
+class Account
+{
+    private String name;
+    private int accountNumber;
+    private double balance;
+    private String addressString;
+    private String accountType;
+
+}
+public Account(String n, int number, double bal)
+{
+    name=n;
+    accountNumber=number;
+    balance=bal;
+}
+public Account(string n, int a, double b, String add,String t)
+{
+    name=n;
+    accountNumber=a;
+    balance=b;
+    addressString=add;
+    accountType=t;
+}
+public class s5 {
+    public static void main(String[] args) {
+        Account obj=new Account(null, 0, 0, null, null)
+        Accoount obj1=new Account(null, 0, 0)
+    }
+    
+}
