42a53bc938bdfc5925b3a2f092e21db78827c900
Jalpanaik <133594529+Jalpanaik@users.noreply.github.com>
2025-05-22 11:34:20 +0000

    v1

---
+++ Applet programs/App1.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class App1 extends Applet{
+    public void piant(Graphics g){
+        g.drawString("Hello World!!!",20,20);
+    }
+}
+    
+/*
+ <applet code="App1"
+ */
